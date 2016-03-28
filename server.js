63bda9bedf3dccba73e5c2b0f502404e48a9525c
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-28 15:51:23 +0300

    changed language

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(middleware.logger);
 //app.use(middleware.requireAuthentication);
 
 app.get('/about', middleware.requireAuthentication , function(req, res){
-	res.send('About Us');
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname+'/public'));
