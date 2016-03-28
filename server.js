374fbe02947703893135c0b4bbc169cb9bd0d690
unknown <z003jn9v@MD1HCHSC.ww300.siemens.net>
2016-03-28 16:00:39 +0300

    Refactor middleware

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
-
-var middleware = {
-	requireAuthentication: function(req, res, next){
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req, res, next) {
-		console.log('Request: '+ new Date() +' '+req.method+ ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
-//app.use(middleware.requireAuthentication);
 
 app.get('/about', middleware.requireAuthentication , function(req, res){
 	res.send('About Us!');
